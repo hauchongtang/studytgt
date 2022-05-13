3fbe5b1412b81babd563e9f5d69a1a715c03213e
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-05-14 00:46:52 +0800

    integrated routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import {NavBar} from './components/nav/navbar'
+import NavBar from './components/nav/navbar'
 import './App.css'
 import React from 'react'
+import { Router, Route, Routes } from 'react-router-dom'
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
+import Home from './components/homepage'
+import { createBrowserHistory } from 'history'
 
+let history = createBrowserHistory()
 const App = () => {
   return (
-    <div>
-      <NavBar/>
-      <h1>Hello </h1>
-    </div>
+    <>
+      <NavBar />
+      <HistoryRouter history={history}>      
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/leaderboard' element={<>leaderboard</>} />
+          <Route path='/calendar' element={<>calendar</>} />
+          <Route path='/planner' element={<>planner</>} />
+        </Routes>
+      </HistoryRouter>
+    </>
   )
 }
 
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,1 +1,14 @@
 import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            {/* <section id='header' className='d-flex align-items-center'>
+                <h1>App Name</h1>
+            </section> */}\
+            <h1>HomePage</h1>
+        </>
+    )
+}
+
+export default Home
