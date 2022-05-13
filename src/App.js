c6af2412875cd2c08ef0c50c32b8b96d3848f5ac
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-05-13 23:29:10 +0800

    navbar responsiveness and headings with href routes added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="container">
+    <div>
       <NavBar/>
       <h1>Hello </h1>
     </div>
--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -1,10 +1,33 @@
-import { Navbar, NavbarBrand, NavItem } from "reactstrap";
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap";
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => setIsOpen(!isOpen)
     return (
         <div id="navbar-top">
-            <Navbar color="faded" light>
+            <Navbar color="faded" light expand="sm">
                 <NavbarBrand href="/" >studytgt</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="me-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/leaderboard">
+                                Leaderboard
+                            </NavLink>
+                        </NavItem>                        <NavItem>
+                            <NavLink href="/calendar">
+                                Calendar
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/planner">
+                                Planner
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
             </Navbar>
         </div>
     )
